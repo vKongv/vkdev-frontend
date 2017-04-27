3f645d4067c2d82adf6416585eb5d432425378d1
Kong Ka Weng <mgongzai@gmail.com>
2017-04-28 00:56:34 +0800

    fix eslint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
   rules: {
       "indent": ["error", 4],
       "import/no-unresolved": "off",
+      "import/no-extraneous-dependencies": "off",
   },
   globals: {}
 }
--- plugins/bootstrap-vue.js
+++ plugins/bootstrap-vue.js
@@ -1,6 +1,6 @@
-import Vue from 'vue'
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
