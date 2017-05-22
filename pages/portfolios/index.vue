0469530a51bdd0bf35fb62ca2c9e4e7872d4c43f
duduvien <vivien@vkdevmy.com>
2017-05-22 23:50:27 +0800

    - Added content to featured news - Added portfolios

--- pages/portfolios/index.vue
+++ pages/portfolios/index.vue
@@ -2,16 +2,16 @@
 <div>
     <web-header />
     <section class="ph5">
-        <div class="fl w-third ph3" v-for="(prtf, index) in portfolios">
+        <div class="fl w-third-l w-100 ph3" v-for="( portfolio, index) in portfolios">
             <article class="br2 ba dark-gray b--black-10 mv3 w-100 center card-container">
-                    <div :style="{ backgroundImage: 'url(' + prtf.img + ')'}" class="db w-100 br2 br--top card-img" :alt="prtf.title"></div>
+                    <div :style="{ backgroundImage: 'url(' +  portfolio.img + ')'}" class="db w-100 br2 br--top card-img" :alt=" portfolio.title"></div>
                     <div class="pa2 ph3-ns pb3-ns card-desc">
                         <div class="dt w-100 mt1">
                             <div class="dtc">
-                                <h1 class="f5 f4-ns mv0 ttu tracked"> {{ prtf.title }} </h1>
+                                <h1 class="f5 f4-ns mv0 ttu tracked"> {{  portfolio.title }} </h1>
                             </div>
                         </div>
-                        <p class="f6 lh-copy measure mt2 mid-gray"> {{ prtf.desc }} </p>
+                        <p class="f6 lh-copy measure mt2 mid-gray"> {{  portfolio.desc }} </p>
                     </div>
                     <!-- <a class="f7 link dim br2 ba ph3 pv3 mb2 dib black ttu tracked" href="#0">View Details »</a> -->
             </article>
@@ -25,28 +25,28 @@
 import WebHeader from '~components/Header.vue';
 import WebFooter from '~components/Footer.vue';
 
-import testPicture from '../../images/featured-news/test-2.png';
+import p1 from '../../images/portfolios/cr.png';
 
 const portfolios = [
     {
-        title: 'Quality',
-        desc: 'We strike for the best quality in every project',
-        img: testPicture,
+        title: 'Lorem Ipsum',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
+        img: p1,
     },
     {
-        title: 'Growth',
-        desc: 'We care about your business growth',
-        img: testPicture,
+        title: 'Lorem Ipsum',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        img: p1,
     },
     {
-        title: 'Collaboration',
-        desc: 'We collaborate with every stakeholder to ensure we align with your business requirements. We collaborate with every stakeholder to ensure we align with your business requirements.',
-        img: testPicture,
+        title: 'Lorem Ipsum',
+        desc: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        img: p1,
     },
     {
-        title: 'Quality',
-        desc: 'We strike for the best quality in every project',
-        img: testPicture,
+        title: 'Lorem Ipsum',
+        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        img: p1,
     },
 ];
 
@@ -64,12 +64,17 @@ export default {
 </script>
 
 <style scoped>
+@import '../../assets/main.css';
 
 .card-container {
     width: 100%;
-    height: 22rem;
+    height: auto;
     text-align: center;
     justify-content: space-between;
+
+    @media(--for-desktop) {
+        height: 22rem;
+    }
 
     &:hover {
         box-shadow: 0 0 20px 3px rgba(0,0,0,0.2);
@@ -77,20 +82,26 @@ export default {
         transition: transform .2s ease-in;
         background-color:  rgba(0,0,0,0.2)
     }
+
 }
 
 .card-img {
-    height: 60%;
     width: 100%;
+    height: 12rem;
     position: center;
-    background-size: cover;
+    background-size: 100%;
     background-repeat: no-repeat;
     background-position: center;
+    background-color: rgba(0, 0, 0, 0.9);
+
+    @media(--for-desktop) {
+        height: 55%;
+    }
 }
 
 .card-desc {
-    height: 35%;
     width: 100%;
+    height: 35%;
 }
 
 </style>
